8e84a3eae81cefc836718f1084a40dc4fd6ff675
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-20 16:56:15 -0400

    fixed errors

--- booked/src/main/java/com/skilldistillery/booked/controllers/UserController.java
+++ booked/src/main/java/com/skilldistillery/booked/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.booked.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.skilldistillery.booked.data.UserDAO;
@@ -7,7 +8,7 @@ import com.skilldistillery.booked.data.UserDAO;
 @Controller
 public class UserController {
 	
-	@AutoWired
+	@Autowired
 	private UserDAO dao;
 
 }
