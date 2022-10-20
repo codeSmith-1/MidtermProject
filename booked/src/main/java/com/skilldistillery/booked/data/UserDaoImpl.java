cac0fc4063069ed93ce6221fae25b6565eaebd03
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-20 18:01:04 -0400

    Full stack smoke test

--- booked/src/main/java/com/skilldistillery/booked/data/UserDaoImpl.java
+++ booked/src/main/java/com/skilldistillery/booked/data/UserDaoImpl.java
@@ -1,11 +1,23 @@
 package com.skilldistillery.booked.data;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.booked.entities.User;
+
 @Service
 @Transactional
 public class UserDaoImpl implements UserDAO {
-
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Override
+	public User findById(int userId) {
+		return em.find(User.class, userId);
+	}
+	
 }
--- booked/src/main/webapp/WEB-INF/home.jsp
+++ booked/src/main/webapp/WEB-INF/home.jsp
@@ -4,12 +4,12 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>booked</title>
 </head>
 <body>
 
 <h2>Hello DBA</h2>
 
-
+${SMOKETEST}
 </body>
 </html>
