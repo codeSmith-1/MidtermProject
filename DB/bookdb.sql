8a8c25e2cee6d9b78a78ceb645dbf5fcbb757f91
Cullen Rafferty <cullen1882@gmail.com>
2022-10-20 20:13:22 -0400

    db updated with tables

--- DB/bookdb.sql
+++ DB/bookdb.sql
@@ -16,6 +16,22 @@ CREATE SCHEMA IF NOT EXISTS `bookdb` DEFAULT CHARACTER SET utf8 ;
 USE `bookdb` ;
 
 -- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `street` VARCHAR(45) NOT NULL,
+  `street2` VARCHAR(45) NULL,
+  `city` VARCHAR(45) NOT NULL,
+  `state` VARCHAR(45) NOT NULL,
+  `zip_code` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -26,8 +42,155 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(45) NOT NULL,
   `enabled` TINYINT(1) NULL,
   `role` VARCHAR(45) NULL,
+  `address_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `author`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `author` ;
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book` ;
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(45) NULL,
+  `rating` DECIMAL(2,1) NULL,
+  `author_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_book_author1_idx` (`author_id` ASC),
+  CONSTRAINT `fk_book_author1`
+    FOREIGN KEY (`author_id`)
+    REFERENCES `author` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `genre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `genre` ;
+
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `shelf_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `shelf_book` ;
+
+CREATE TABLE IF NOT EXISTS `shelf_book` (
+  `id` INT NOT NULL,
+  `condition` VARCHAR(45) NOT NULL,
+  `availability` VARCHAR(45) NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_shelf_book_book1_idx` (`book_id` ASC),
+  CONSTRAINT `fk_shelf_book_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book_has_genre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book_has_genre` ;
+
+CREATE TABLE IF NOT EXISTS `book_has_genre` (
+  `book_id` INT NOT NULL,
+  `genre_id` INT NOT NULL,
+  PRIMARY KEY (`book_id`, `genre_id`),
+  INDEX `fk_book_has_genre_genre1_idx` (`genre_id` ASC),
+  INDEX `fk_book_has_genre_book1_idx` (`book_id` ASC),
+  CONSTRAINT `fk_book_has_genre_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_has_genre_genre1`
+    FOREIGN KEY (`genre_id`)
+    REFERENCES `genre` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `comments`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `comments` ;
+
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `comment` LONGTEXT NOT NULL,
+  `user_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_comments_user1_idx` (`user_id` ASC),
+  INDEX `fk_comments_book1_idx` (`book_id` ASC),
+  CONSTRAINT `fk_comments_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comments_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_has_shelf_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_has_shelf_book` ;
+
+CREATE TABLE IF NOT EXISTS `user_has_shelf_book` (
+  `user_id` INT NOT NULL,
+  `shelf_book_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `shelf_book_id`),
+  INDEX `fk_user_has_shelf_book_shelf_book1_idx` (`shelf_book_id` ASC),
+  INDEX `fk_user_has_shelf_book_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_shelf_book_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_shelf_book_shelf_book1`
+    FOREIGN KEY (`shelf_book_id`)
+    REFERENCES `shelf_book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -42,11 +205,21 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `address`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `bookdb`;
+INSERT INTO `address` (`id`, `street`, `street2`, `city`, `state`, `zip_code`) VALUES (1, '1503 Oak Knoll Lane', NULL, 'Virginia Beach', 'VA', '23464');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `bookdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', 'admin', NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `address_id`) VALUES (1, 'admin', 'admin', 1, NULL, 1);
 
 COMMIT;
 
