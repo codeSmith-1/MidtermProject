5f9ab72e0039d1247324af17925aa7b7f88fa08c
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-21 14:12:52 -0400

    stub entities, add user fields

---
+++ JPAbooked/src/main/java/com/skilldistillery/booked/entities/Rating.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.booked.entities;
+
+public class Rating {
+
+}
--- JPAbooked/src/main/java/com/skilldistillery/booked/entities/User.java
+++ JPAbooked/src/main/java/com/skilldistillery/booked/entities/User.java
@@ -2,6 +2,7 @@ package com.skilldistillery.booked.entities;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,16 +16,30 @@ public class User {
 	private int id;
 
 	private String username;
-
+	
 	private String password;
+
+	private String email;
 
 	private Boolean enabled;
 
 	private String role;
+	
+	@Column(name="address_id")
+	private int addressId;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	private String description;
+	
+	@Column(name="profile_img")
+	private String profileImg;
 
-	public User() {
-		super();
-	}
+	public User() {}
 	
 	public User(String username, String password, boolean enabled, String role) {
 		this.username = username;
@@ -71,6 +86,54 @@ public class User {
 
 	public void setRole(String role) {
 		this.role = role;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getProfileImg() {
+		return profileImg;
+	}
+
+	public void setProfileImg(String profileImg) {
+		this.profileImg = profileImg;
 	}
 
 	@Override
