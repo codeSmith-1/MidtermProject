d02ff5ce4b97434e65f9acb1ecc394218b66463d
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-22 17:31:54 -0400

    no change

--- booked/src/main/java/com/skilldistillery/booked/data/UserDaoImpl.java
+++ booked/src/main/java/com/skilldistillery/booked/data/UserDaoImpl.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.booked.entities.User;
