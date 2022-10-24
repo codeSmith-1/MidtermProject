4ff1d830acbb58c3661e95e66cd387b6d9274e43
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-24 15:14:19 -0400

    add bookdaoimpl and methods

--- booked/src/main/java/com/skilldistillery/booked/data/BookDAO.java
+++ booked/src/main/java/com/skilldistillery/booked/data/BookDAO.java
@@ -1,8 +1,13 @@
 package com.skilldistillery.booked.data;
 
+import java.util.List;
+
+import com.skilldistillery.booked.entities.Comment;
+import com.skilldistillery.booked.entities.Rating;
+
 public interface BookDAO {
 
-	
-	
-	
+	List<Rating> getRatingsByBookId(int bId);
+	List<Comment> getCommentsByBookId(int bId);
+
 }
